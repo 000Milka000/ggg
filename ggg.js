502defc08bfd2bc888a07890571acfed14d80d02
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-10-23 23:18:09 +0300

    TYUI

--- ggg.js
+++ ggg.js
@@ -5,3 +5,9 @@ a.forEach((i) => {console.log(i^2)});
 console.log("")
 a.forEach(console.log);
 a.forEach(console.log)
+a.forEach((i) => {console.log(i**2)});
+console.log("")
+a.forEach((i) => {console.log(i^2)});
+console.log("")
+a.forEach(console.log);
+a.forEach(console.log)
