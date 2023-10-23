fb0a4b90fe2e177ad29aa705021023511c314729
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-10-23 23:15:43 +0300

    Фиксация 2.0

--- ggg.js
+++ ggg.js
@@ -1,7 +1,10 @@
 let a = [22,44,55,77];
 a.forEach((i) => {console.log(i**2)});
 console.log("")
-a.forEach((i) => {console.log(i^2)});
-console.log("")
+
 a.forEach(console.log);
+
+console.log("")
+
+
 a.forEach(console.log)
